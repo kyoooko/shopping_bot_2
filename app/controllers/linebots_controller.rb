23489879d09487426932307bab9f240c7a0249fd
飯田恭子 <iidakyouko@mba.local>
2020-05-19 20:57:59 +0900

    correct linebots_controller.rb

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -118,7 +118,7 @@ class LinebotsController < ApplicationController
             "style": "primary",
             "action": {
               "type": "uri",
-              "label": "Amazon商品ページへ",
+              "label": "楽天市場商品ページへ",
               "uri": url
             }
           }
